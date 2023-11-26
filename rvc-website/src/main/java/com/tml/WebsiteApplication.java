757955c9290c7c81d70d5e10676dad9691d3a1b8
userA <geniusssbg@gmail.com>
2023-11-26 14:12:05 +0800

    init

---
+++ rvc-website/src/main/java/com/tml/WebsiteApplication.java
@@ -0,0 +1,14 @@
+package com.tml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class WebsiteApplication {
+    public static void main( String[] args )
+    {
+        SpringApplication.run(WebsiteApplication.class, args);
+    }
+}
