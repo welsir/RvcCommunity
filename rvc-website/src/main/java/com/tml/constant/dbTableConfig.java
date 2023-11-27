25ec09a971c6526a4e7688ab3a4a5948498d0258
userA <geniusssbg@gmail.com>
2023-11-28 01:44:07 +0800

    [Dev：RVC网站服务] 实现团队人员信息接口

--- rvc-website/src/main/java/com/tml/constant/dbTableConfig.java
+++ rvc-website/src/main/java/com/tml/constant/dbTableConfig.java
@@ -4,4 +4,5 @@ public class dbTableConfig {
 
     public static final String RVC_WEB_INFO = "rvc_web_info";
     public static final String RVC_WEB_TOOL = "rvc_web_tool";
+    public static final String RVC_WEB_TEAM = "rvc_web_team";
 }
---
+++ rvc-website/src/main/java/com/tml/mapper/TeamMapper.java
@@ -0,0 +1,7 @@
+package com.tml.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.tml.pojo.TeamDO;
+
+public interface TeamMapper extends BaseMapper<TeamDO> {
+}
