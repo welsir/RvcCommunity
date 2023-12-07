68d0983b2f4fdc3ac4633ebb5147db977dd15775
dct <2382056614@qq.com>
2023-12-08 00:43:28 +0800

    更新雪花id生成器，修复网关启动报错

---
+++ rvc-gateway/src/main/java/com/tml/config/RedisConfig.java
@@ -0,0 +1,12 @@
+package com.tml.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+
+
+}
