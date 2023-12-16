c9fc27f986907a252ceff2c71c6cf9b55c22571a
welsir <1824379011@qq.com>
2023-12-16 17:15:48 +0800

    [Dev:模型模块] 模型模块上线 <Auto> -e:test -m:rvc-model -v:1.9.1 -rp:9100 -de:<-e ACTIVE=dev -e UNIQUE_ID=0 -e SERVER_PORT=9100>

--- rvc-model/src/main/java/com/tml/core/client/UserServiceClient.java
+++ rvc-model/src/main/java/com/tml/core/client/UserServiceClient.java
@@ -1,14 +1,11 @@
 package com.tml.core.client;
 
 import com.tml.common.Result;
-import com.tml.config.FeignConfig;
 import com.tml.constant.RemoteModuleURL;
 import com.tml.pojo.DTO.UserInfoDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-
-import javax.activation.MimeType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 /**
@@ -19,7 +16,7 @@ import javax.activation.MimeType;
 @FeignClient(name = "tml-user-service")
 public interface UserServiceClient {
 
-    @GetMapping(value = RemoteModuleURL.GET_USERINFO,consumes = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = RemoteModuleURL.GET_USERINFO)
     Result<UserInfoDTO> getUserInfo(@RequestParam("uid") String uid);
 
 }
