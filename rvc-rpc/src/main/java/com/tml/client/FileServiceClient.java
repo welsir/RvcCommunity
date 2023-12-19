7488b4af0dfa4c9afe9ff54e01b8bd5e5b1ab7cd
welsir <1824379011@qq.com>
2023-12-20 02:03:38 +0800

    [Dev:模型模块] bug修复2.4.4.1 <Auto> -e:test -m:rvc-model -v:1.9.7.4.2 -rp:9100 -de:<-e ACTIVE=dev -e UNIQUE_ID=0 -e SERVER_PORT=9100>

--- rvc-rpc/src/main/java/com/tml/client/FileServiceClient.java
+++ rvc-rpc/src/main/java/com/tml/client/FileServiceClient.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
  * @Author welsir
  * @Date 2023/12/6 16:01
  */
-@FeignClient(name = "rvc_file_service",configuration = FeignConfig.class)
+@FeignClient(name = "rvc-file-service",configuration = FeignConfig.class)
 public interface FileServiceClient {
 
     @PostMapping(value = RemoteModuleURL.UPLOAD_FILE_TO_OSS,consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
