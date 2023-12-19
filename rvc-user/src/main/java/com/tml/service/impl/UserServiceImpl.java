12bd9762cd86eb0e4a35184d7069c5c455ad5599
爪哇程序猿 <3250162035@qq.com>
2023-12-19 18:46:34 +0800

    [Dev:用户模块]<Auto> -e:test -m:rvc-user -v:1.0.7 -rp:9000 -de:<-e ACTIVE=test -e SERVER_PORT=9000> "用户模块删除无用数据库字段，添加新接口exist"

--- rvc-user/src/main/java/com/tml/service/impl/UserServiceImpl.java
+++ rvc-user/src/main/java/com/tml/service/impl/UserServiceImpl.java
@@ -215,6 +215,7 @@ public class UserServiceImpl implements UserService {
                 userInfoVO.setNickname(user.getUsername());
                 userInfoVO.setAvatar(userInfoVO.getAvatar());
             }
+
             userList.add(userInfoVO);
         }
         return userList;
