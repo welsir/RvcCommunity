d8bdc0c682cda5ff03dd48a753d611104b2a40db
userA <geniusssbg@gmail.com>
2023-12-20 20:43:57 +0800

    [Prod 网关模块] 上线网站服务 <Auto> -e:prod -m:rvc-website -v:1.0 -rp:9300 -de:<-e ACTIVE=prod -e UNIQUE_ID=0 -e SERVER_PORT=9300>

--- rvc-feedback/src/main/java/com/tml/config/RvcExceptionHandler.java
+++ rvc-feedback/src/main/java/com/tml/config/RvcExceptionHandler.java
@@ -3,12 +3,14 @@ package com.tml.config;
 import com.tml.exception.RvcSQLException;
 import io.github.common.logger.CommonLogger;
 import io.github.common.web.Result;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class RvcExceptionHandler {
@@ -21,6 +23,7 @@ public class RvcExceptionHandler {
     public Result SqlErrorHandler(HttpServletRequest httpServletRequest, Exception e){
         logger.error("数据库异常:{}",e.getMessage());
         return Result.error("503",e.getMessage());
+
     }
 
 }
