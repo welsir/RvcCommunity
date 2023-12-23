4efc61723065b19584c6abd03a647afac7908a06
yue <3391855334@qq.com>
2023-12-23 21:36:18 +0800

    [Test:交流区] 获取帖子没有封面bug <Auto> -e:test -m:rvc-communication -v:1.3.1 -rp:9200 -de:<-e ACTIVE=test -e UNIQUE_ID=0 -e SERVER_PORT=9200>

--- rvc-communication/src/main/java/com/tml/service/impl/PostServiceImpl.java
+++ rvc-communication/src/main/java/com/tml/service/impl/PostServiceImpl.java
@@ -197,6 +197,7 @@ public class PostServiceImpl extends ServiceImpl<PostMapper, Post> implements Po
         Cover cover = coverMapper.selectById(post.getCoverId());
         postVo.setCover(cover.getCoverUrl());
 
+
 //        如果用户未登录直接返回vo对象
         if (Objects.isNull(uid)){
             return postVo;
