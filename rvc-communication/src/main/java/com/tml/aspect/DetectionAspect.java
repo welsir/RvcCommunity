cebd429fa0a680bc5e6c2d885a3670c7e88bc081
yue <3391855334@qq.com>
2023-12-23 17:25:56 +0800

    [Test:交流区] 评论bug <Auto> -e:test -m:rvc-communication -v:1.3 -rp:9200 -de:<-e ACTIVE=test -e UNIQUE_ID=0 -e SERVER_PORT=9200>

--- rvc-communication/src/main/java/com/tml/aspect/DetectionAspect.java
+++ rvc-communication/src/main/java/com/tml/aspect/DetectionAspect.java
@@ -95,6 +95,8 @@ public class DetectionAspect {
                         }
                     }
                 }
+
+
                 DetectionTaskDto textDetectionTaskDto = DetectionTaskDto.builder()
                         .id(uuid)
                         .content(contentValue)
