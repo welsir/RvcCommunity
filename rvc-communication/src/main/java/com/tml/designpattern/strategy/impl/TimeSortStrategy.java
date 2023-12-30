171af1022cea52e93b7a38f85e8b04ae4ed032aa
yue <3391855334@qq.com>
2023-12-30 17:43:55 +0800

    [Test:交流区服务] bug修复 <Auto> -e:prod -m:rvc-communication -v:2.0 -rp:9200 -de:<-e ACTIVE=prod -e UNIQUE_ID=0 -e SERVER_PORT=9200>

--- rvc-communication/src/main/java/com/tml/designpattern/strategy/impl/TimeSortStrategy.java
+++ rvc-communication/src/main/java/com/tml/designpattern/strategy/impl/TimeSortStrategy.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class TimeSortStrategy implements SortStrategy {
     @Override
     public void sort(List<PostVo> posts) {
+
         // 根据时间排序
         posts.sort(Comparator.comparing(PostVo::getCreateAt));
     }
