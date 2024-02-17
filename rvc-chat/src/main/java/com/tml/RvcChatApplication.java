1b5f0309881d6b2ba34dd90c06d986e4ac3a678a
welsir <1824379011@qq.com>
2024-02-17 16:23:09 +0800

    [DEV:模型模块] 模型模块上线测试服务器 <Auto> -e:test -m:rvc-model -v:1.3 -rp:9100 -dp:9101 -de:<-e ACTIVE=dev -e UNIQUE_ID=0 -e SERVER_PORT=9100>

---
+++ rvc-chat/src/main/java/com/tml/RvcChatApplication.java
@@ -0,0 +1,13 @@
+package com.tml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RvcChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RvcChatApplication.class, args);
+    }
+
+}
