0630a93e3fa37ebaa48a5785515c34225a2e2858
welsir <1824379011@qq.com>
2024-02-19 16:44:38 +0800

    [DEV:模型模块] 抽离netty <Auto> -e:test -m:rvc-model -v:1.3 -rp:9100 -dp:9101 -de:<-e ACTIVE=dev -e UNIQUE_ID=0 -e SERVER_PORT=9100>

---
+++ rvc-chat/src/main/java/com/tml/controller/ChatController.java
@@ -0,0 +1,28 @@
+package com.tml.controller;
+
+import com.tml.pojo.Result;
+import com.tml.pojo.vo.ChatRoomRequest;
+import com.tml.pojo.vo.RoomDetailInfoVO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Description
+ * @Author welsir
+ * @Date 2024/2/19 15:53
+ */
+@RequestMapping("/room")
+public class ChatController {
+
+    /**
+     * @description: 创建房间
+     * @param: chatRoomRequest
+     * @return: Result<RoomDetailInfoVO>
+     **/
+    @PostMapping("/create")
+    public Result<RoomDetailInfoVO> createRoom(@RequestBody ChatRoomRequest chatRoomRequest){
+
+    }
+
+}
---
+++ rvc-chat/src/main/java/com/tml/initializer/WebSocketServerInitializer.java
@@ -0,0 +1,12 @@
+package com.tml.initializer;
+
+/**
+ * @Description websocket连接初始化Channel
+ * @Author welsir
+ * @Date 2024/2/19 15:36
+ */
+public class WebSocketServerInitializer {
+
+
+
+}
