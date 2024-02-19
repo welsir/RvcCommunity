ac7977bb614236fc4b088613b62fc1159f4c01e9
welsir <1824379011@qq.com>
2024-02-19 16:49:04 +0800

    抽离netty

---
+++ rvc-netty/src/main/java/com/tml/netty/RvcNettyApplication.java
@@ -0,0 +1,13 @@
+package com.tml.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RvcNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RvcNettyApplication.class, args);
+    }
+
+}
---
+++ rvc-netty/src/test/java/com/tml/netty/RvcNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tml.netty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RvcNettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
