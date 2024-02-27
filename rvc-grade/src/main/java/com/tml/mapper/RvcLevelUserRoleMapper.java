2b943423a3d5810675ad3d0ee0f57081e086d701
yue <3391855334@qq.com>
2024-02-27 19:40:10 +0800

    【等级系统】实现权限校验aop注解

--- rvc-grade/src/main/java/com/tml/mapper/RvcLevelUserRoleMapper.java
+++ rvc-grade/src/main/java/com/tml/mapper/RvcLevelUserRoleMapper.java
@@ -1,15 +1,15 @@
-package com.tml.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.tml.domain.entity.RvcLevelUserRole;
-
-/**
-* @author yuech
-* @description 针对表【rvc_level_user_role】的数据库操作Mapper
-* @createDate 2024-01-30 20:58:57
-* @Entity com.tml.domain.entity.RvcLevelUserRole
-*/
-public interface RvcLevelUserRoleMapper extends BaseMapper<RvcLevelUserRole> {
-
-
-}
+//package com.tml.mapper;
+//
+//import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+//import com.tml.domain.entity.RvcLevelUserRole;
+//
+///**
+//* @author yuech
+//* @description 针对表【rvc_level_user_role】的数据库操作Mapper
+//* @createDate 2024-01-30 20:58:57
+//* @Entity com.tml.domain.entity.RvcLevelUserRole
+//*/
+//public interface RvcLevelUserRoleMapper extends BaseMapper<RvcLevelUserRole> {
+//
+//
+//}
