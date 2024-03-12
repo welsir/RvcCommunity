3e206a5bf626d7e3606f4c55fa272f91b04e3560
welsir <1824379011@qq.com>
2024-03-13 02:15:45 +0800

    [DEV:聊天室] 聊天室测试连接建立 <Auto> -e:test -m:rvc-chat -v:1.0 -rp:9110 -de<-e ACTIVE=test -e SERVER_PORT = 9110>

--- rvc-chat/src/main/java/com/tml/config/NettyServerConfiguration.java
+++ rvc-chat/src/main/java/com/tml/config/NettyServerConfiguration.java
@@ -2,6 +2,8 @@ package com.tml.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Component;
  */
 @Data
 @Component
+@Configuration
 public class NettyServerConfiguration {
 
     @Value("${channel.handshake.wait}")
